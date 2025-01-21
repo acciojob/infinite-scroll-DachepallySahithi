ca20143d33a2cbbe97cf6f5dd89ce9ae0372f563
DachepallySahithi <130071155+DachepallySahithi@users.noreply.github.com>
2025-01-21 19:07:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,21 @@
 //your code here!
 
+function generateListItems(count) {
+	const list = document.getElementById('infi-list');
+
+	for (let i = 0; i < count; i++) {
+    const li = document.createElement('li');
+    li.textContent = `Item ${list.children.length + 1}`;
+    list.appendChild(li);
+  }
+}
+
+function checkScroll() {
+  const list = document.getElementById('infi-list');
+  
+  if (list.scrollHeight - list.scrollTop === list.clientHeight) {
+    generateListItems(2);
+  }
+}
+generateListItems(10);
+document.getElementById('infi-list').addEventListener('scroll', checkScroll);
